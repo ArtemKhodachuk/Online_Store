0a43e1250fa664e3259f6d1fbc07975f1ae967a4
Artem Khodachuk <akhoda@softserveinc.com>
2023-04-08 18:07:55 -0700

    upd

--- online_store/product_cart/urls.py
+++ online_store/product_cart/urls.py
@@ -6,4 +6,6 @@ from . import views
 app_name = 'product_cart'
 urlpatterns = [
     path('user/int:pk/cart', views.UserCartView.as_view()), # Return the cart of the user
+    path('cart/int:cart_id/cartitem', views.CartView.as_view()),
+    path('/cart/int:cart_id/cartitem/int:pk', views.InCartProductView.as_view()) # Correct??? Why use cart_id??
 ]
--- online_store/product_cart/views.py
+++ online_store/product_cart/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from product_cart.permissions import IsOwner
 # Create your views here.
 from product_cart.serializers import CartSerializer
+from product_cart.models import Cart, ProductInCart
 
 #View for returning the user cart (Correct???????)
 class UserCartView(generics.RetrieveUpdateDestroyAPIView):
@@ -13,3 +14,16 @@ class UserCartView(generics.RetrieveUpdateDestroyAPIView):
     def get_object(self):
         pk = self.kwargs.get('pk')
         return Userprofile.objects.get(pk=pk).cart
+
+class CartView(generics.ListCreateAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = ProductInCart
+    def get_queryset(self):
+        cart_id = self.kwargs.get('cart_id')
+        return Cart.objects.get(pk=cart_id).productincart_set
+
+# Correct if ingnoring cart_id?
+class InCartProductView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductInCart.objects.all()
+    serializer_class = ProductInCart
+    
