6e7c03cbf08adc76d7595c86d8c2e53f9722cfdd
Artem Khodachuk <akhoda@softserveinc.com>
2023-05-22 12:25:09 -0700

    Added the search libs

--- online_store/online_store/settings.py
+++ online_store/online_store/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'product',
     'order_info',
     'transaction_info',
+    'django_filters',
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
